1da06e078ac1ee1c54028cf0227fd80b32b91159
VincentCarlino <vjcarlino22@gmail.com>
2019-02-26 00:03:21 -0500

    Part 3

---
+++ Assets/Levitate.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Levitate : MonoBehaviour
+{
+    private bool fly = false;
+    public bool canFly = true;
+
+    public float hoverForce = 15f;
+    public float hoverHeight = 1.45f;
+
+    private Rigidbody rb;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(fly && canFly)
+        {
+            Ray ray = new Ray(transform.position, -transform.up);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, hoverHeight))
+            {
+                float proportionalHeight = (hoverHeight - hit.distance) / hoverHeight;
+                Vector3 appliedHoverForce = Vector3.up * proportionalHeight * hoverForce;
+                rb.AddRelativeForce(appliedHoverForce, ForceMode.Acceleration);
+            };
+        }
+
+    }
+
+    public void SetFly(bool f)
+    {
+        fly = f;
+    }
+}
---
+++ Assets/Wand.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wand : MonoBehaviour
+{
+    public float range = 100f;
+    public Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot("fly");
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Shoot("fall");
+        }
+    }
+
+    void Shoot(string command)
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(camera.transform.position, camera.transform.forward, out hit, range))
+        {
+            if(command == "fly")
+            {
+                hit.transform.GetComponent<Levitate>().SetFly(true);
+            }
+            else if (command == "fall")
+            {
+                hit.transform.GetComponent<Levitate>().SetFly(false);
+            }
+            
+        };
+    }
+
+    
+}
